886dd1742bb8f52fa9c9c5f599a43a552c612cc2
Alicia G <jwgirl1200@hotmail.com>
2025-09-19 00:28:44 -0400

    Clean up of professor.py
    Had to match the proper formatting of code outlined in course.py.

--- labs/lab-3/db/schema/professor.py
+++ labs/lab-3/db/schema/professor.py
@@ -4,19 +4,22 @@ from db.server import db
 class Professor(db.Model):
     __tablename__ = 'Professors'
     ProfessorID = db.Column(db.Integer,primary_key=True, autoincrement=True)
-    FirstName = db.Column(db.String(50), nullable=False)
-    LastName = db.Column(db.String(50), nullable=False)
-    Email = db.Column(db.String(120), unique=True, nullable=False)
+    FirstName = db.Column(db.String(50))
+    LastName = db.Column(db.String(50))
+    Email = db.Column(db.String(120))
 
     # create relationship with courses table. assoc table name = ProfessorCourse
     course = db.relationship('Courses', secondary = 'ProfessorCourse', back_populates = 'Professors')
-    def __init__(self, first_name, last_name, email):
-       self.FirstName = first_name
-       self.LastName = last_name
-       self.Email = email
+    def __init__(self, name):
+       self.FirstName = self.FirstName
+       self.LastName = self.LastName
+       self.Email = self.Email
 
     def __repr__(self):
-        return f"<Professor {self.ProfessorID}: {self.FirstName} {self.LastName}, Email={self.Email}>"
-    
+        return f"""
+            "FIRST NAME: {self.FirstName}
+            "LAST NAME: {self.LastName}
+            "EMAIL: {self.Email}
+        """
     def __repr__(self):
         return self.__repr__()
