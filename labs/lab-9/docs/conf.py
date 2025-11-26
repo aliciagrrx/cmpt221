6fab35f0882b8f432ac0cb25de5bead315af5bf5
Alicia G <jwgirl1200@hotmail.com>
2025-11-26 01:38:24 -0500

    Revert "issue was in the conf.py"
    This reverts commit 60cb2d1ca674d658245bd258c87a5493d42118ff.

--- labs/lab-9/docs/conf.py
+++ labs/lab-9/docs/conf.py
@@ -26,11 +26,3 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
-html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',   # <-- THIS is what makes your pages show up
-        'relations.html',
-        'searchbox.html',
-    ]
-}
